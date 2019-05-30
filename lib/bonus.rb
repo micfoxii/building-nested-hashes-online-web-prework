5107a219f368a6631a9e1745b0c8885f84451820
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-30 17:36:43 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,8 +20,9 @@ def bonus
    }
   }
 
-  #code your solution here:
-puts epic_tragedy[:status] = "dead"
+  #code your solution here:[]
+  new_status = "dead"
+epic_tragedy
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
